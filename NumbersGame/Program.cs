62d7bddabb7c62e190e8519803f461e4c9a1cdd7
norwinz <dan-gustafsson@hotmail.com>
2021-09-28 11:38:03 +0200

    Test

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace NumbersGame
 {
@@ -6,70 +7,143 @@ namespace NumbersGame
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            string low;
-            string high;
-            int lowNumber;
-            int highNumber;
+            
+            bool playAgain = true;
+            while (playAgain == true)
+            { 
+            
+            string lowString = "placeholder";
+            string highString = "placeholder";
+            int lowNumber = 0;
+            int highNumber = 0;
             bool lowTest = false;
-
-            Console.WriteLine("Välkommen till det superkluriga gissa-rätt-nummer-spelet!");
-            Console.WriteLine("Först måste du välja vilka nummer du vill gissa imellan.");
-            //Chose low number 
-            //for (lowTest == false)
-            //{
-                Console.WriteLine("Lägst nummer: ");
-                low = Console.ReadLine();
-                lowNumber = Convert.ToInt32(low);
-                //bool lowTrue = int.TryParse(lowNumber);
-            //}
-           //Chose high number
-            Console.WriteLine("Högst nummer: ");
-            high = Console.ReadLine();
-            highNumber = Convert.ToInt32(high);
-            
-
-            int randNumber = rand.Next(lowNumber, highNumber);
+            bool highTest = false;
+            bool toLow = false;
             string guessText;
+            bool textSvar = false;
             
             
+            
+                //Welcome text
+                Console.WriteLine("Välkommen till det superkluriga gissa-rätt-nummer-spelet!");
+                Console.WriteLine("Först måste du välja vilka nummer du vill gissa imellan.");
 
-            Console.WriteLine("Välkommen! Jag tänker på ett nummer mellan {0} och {1}. " +
-                "Kan du gissa vilket? Du får fem försök.", lowNumber, highNumber);
-
-            for (int amountGuess = 0; amountGuess < 5; amountGuess++)
-            {
-                if (amountGuess <= 6)
+                //Chose low number
+                while (lowTest == false)
                 {
-                    guessText = Console.ReadLine();
-                    int guessNumber = Convert.ToInt32(guessText);
-
-                    if (guessNumber == randNumber)
+                    Console.WriteLine("Lägst nummer: ");
+                    lowString = Console.ReadLine();
+                    if (int.TryParse(lowString, out lowNumber))
                     {
-                        Console.WriteLine("Woho! Du gjorde det!");
-                        amountGuess = amountGuess + 100; //End the amountGuess loop
+                        lowTest = true;
                     }
-                    else if (guessNumber < randNumber)
+                    else
                     {
-                        Console.WriteLine("Tyvärr, du gissade för lågt!");
-
+                        Console.WriteLine("Du måste skriva en siffra!");
                     }
-                    else if (guessNumber > randNumber)
-                    {
-                        Console.WriteLine("Tyvärr, du gissade för högt!");
+                }
 
-                        //Four IFs, correct, to low, to high or no more guesses 
-                    }  
+                //Chose high number
+                while (highTest == false || toLow == false)
+                {
+                    Console.WriteLine("Högst nummer: ");
+                    highString = Console.ReadLine();
+                    if (int.TryParse(highString, out highNumber))
+                    {
+                        highTest = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du måste skriva en siffra!");
+                    }
+                    if (lowNumber < highNumber)
+                    {
+                        toLow = true;
+                    }
+                    else
+                    {
+                        if (highTest == true)
+                        {
+                            Console.WriteLine("Talet måste vara högre än ditt lägsta tal!");
+                        }
+                    }
+                }
+
+                int randNumber = Randomize(lowNumber, highNumber); //Call method
+
+                //Start of the game
+                Console.WriteLine("Välkommen! Jag tänker på ett nummer mellan {0} och {1}. " +
+                        "Kan du gissa vilket? Du får fem försök.", lowNumber, highNumber);
+
+                for (int amountGuess = 0; amountGuess < 5; amountGuess++)
+                {
+                    if (amountGuess <= 6)
+                    {
+                        guessText = Console.ReadLine();
+                        int guessNumber = Convert.ToInt32(guessText);
+
+                        if (guessNumber == randNumber)
+                        {
+                            Console.WriteLine("Woho! Du gjorde det!");
+                            amountGuess = amountGuess + 100; //End the amountGuess loop
+                        }
+                        else if (guessNumber < randNumber)
+                        {
+                            Console.WriteLine("Tyvärr, du gissade för LÅGT!");
+                        }
+                        else if (guessNumber > randNumber)
+                        {
+                            Console.WriteLine("Tyvärr, du gissade för HÖGT!");
+                        }
                         if (amountGuess >= 5 && randNumber != guessNumber)
-                    {
-                        Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök");
-                    }
+                        {
+                            Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök");
+                        }
 
+
+                    }
 
                 }
-                
+                Console.WriteLine("Spelet är nu slut!");
+                while (textSvar == false)
+                {
+                    Console.WriteLine("Vill du spela igen? (Ja/Nej)");
+                    string igenString = Console.ReadLine().ToUpper();
+                    if (igenString == "NEJ")
+                    {
+                        playAgain = false;
+                        textSvar = true;
+                    }
+                    else if (igenString == "JA")
+                    {
+                        textSvar = true;
+                        Console.Write("Startar ett nytt spel om: ");
+                        for(int i=5; i>0; i--)
+                        {
+                            
+                          Console.Write(i);
+                           
+                            Thread.Sleep(600);
+                            Console.Write("\b");
+                        }
+                        
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Skriv Ja eller Nej");
+                    }
+
+                }
             }
-            Console.WriteLine("Spelet är nu slut!");
+            }
+            public static int Randomize(int numLow, int numHigh)
+            {
+                Random rand = new Random();
+                numHigh++; //Add one to fix rand.next command
+                return rand.Next(numLow, numHigh);
+
+            }
+            
         }
     }
-}
